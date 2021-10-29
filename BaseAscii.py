53a5f35575911ef9a506c2d1f384b53eb2d6abe7
lironmele <lironmeler@gmail.com>
2021-10-29 20:07:06 +0000

    Rename functions and add support for addition

--- BaseAscii.py
+++ BaseAscii.py
@@ -1,13 +1,13 @@
 class BaseAscii:
     def __init__(self, string):
         self.string = string
-        self.base26_list = self._to_ascii()
-        self.number = self._base_10_to_26()
+        self.base26_list = self._to_base_26_list()
+        self.number = self._base_26_to_10()
     
-    def _to_ascii(self):
+    def _to_base_26_list(self):
         return list(map(lambda c: ord(c) - 97, self.string))
 
-    def _base_10_to_26(self):
+    def _base_26_to_10(self):
         base10 = 0
         n = len(self.base26_list) - 1
 
@@ -16,6 +16,25 @@ class BaseAscii:
             n -= 1
 
         return base10
+
+    def _to_ascii(number):
+        string = ''
+        result = number
+        remainder = number % 26
+        n = 0
+
+        while result != 0:
+            string = chr(remainder + 97) + string
+            result //= 26 
+            remainder = result % 26
+            n += 1
+
+        return string
+
+    def __add__(self, x):
+        string = BaseAscii._to_ascii(self.number + x)
+        string = string.rjust(len(self.string), 'a')
+        return string
 
     def __sub__(self, x):
         if self.number > x.number:
@@ -31,9 +50,14 @@ class BaseAscii:
 
 def main():
     start = BaseAscii("aaa")
-    stop = BaseAscii("bbd")
+    stop = BaseAscii("aaz")
 
-    print(start-stop)
+    count = stop - start
+    print(count)
+
+    half = BaseAscii(start+count//2)
+
+    print(stop - half)
 
 if __name__ == "__main__":
     main()
