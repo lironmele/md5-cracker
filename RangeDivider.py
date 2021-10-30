f26b7bf24d0699ced84fee86c70d46a9be7ec429
lironmele <lironmeler@gmail.com>
2021-10-30 02:22:17 +0000

    Rename class to better describe its purpose

--- RangeDivider.py
+++ RangeDivider.py
@@ -1,8 +1,11 @@
 import BaseAscii
 
-class RangeDivider:
+class Range:
     def __init__(self, start='aaaaaa', stop='zzzzzz', range_count=10):
-        pass
+        self.start = BaseAscii.BaseAscii(start)
+        self.stop = BaseAscii.BaseAscii(stop)
+        self.range_count = range_count
+        self.ranges = self / self.range_count
 
     def __div__(self, x):
         pass
