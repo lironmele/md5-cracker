2ffbf4c17bb7e492991a36628711c7d5e6e1d7fa
lironmele <lironmeler@gmail.com>
2021-10-30 13:39:24 +0000

    Create a client class and handle giving out ranges

--- Server/server.py
+++ Server/server.py
@@ -2,12 +2,16 @@ import socket
 import threading
 from typing import List
 
+import BaseAscii
+import RangeDivider
+
 ip = "0.0.0.0"
 port = 13370
 
 class Server:
     def __init__(self):
         self.clients = []
+        self.ranges = []
         self.soc = socket.socket()
         self.id_count = 1
 
@@ -29,14 +33,47 @@ class Server:
 
             self.id_count += 1
 
-    def add_new_patz(self, id, client_addr):
-        client = socket.socket()
-        client.connect((client_addr, port+id))
+    def add_new_patz(self, c_id, client_addr):
+        client = Client(c_id=c_id, manager=self, range=self.get_range())
+        client.connect((client_addr, port+c_id))
 
         self.clients.append(client)
+
+        client.communicate()
+
+    def get_range(self, urange=None):
+        if urange == None:
+            for r in self.ranges:
+                if not r:
+                    return self.get_range(r)
+        else:
+            for r in urange.ranges:
+                if not r:
+                    return self.get_range(r)
+            return urange
 
     def handle_clients(self):
         pass
 
     def send_horray(self):
+        pass
+
+class Client(socket.socket):
+    def __init__(self, *args, **kwargs) -> None:
+        super(Client, self).__init__(*args, **kwargs)
+        
+        self.c_id = kwargs["c_id"]
+        self.manager: Server = kwargs["manager"]
+        self.range = kwargs["range"]
+        self.md5 = ""
+        self.searching = False
+
+    @classmethod
+    def copy(cls, sock):
+        fd = socket.dup(sock.fileno())
+        copy = cls(sock.family, sock.type, sock.proto, fileno=fd)
+        copy.settimeout(sock.gettimeout())
+        return copy
+
+    def communicate(self):
         pass
