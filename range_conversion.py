f377e4920b3691ac3a4b9f84cc4b182afed768bf
lironmele <lironmeler@gmail.com>
2021-11-04 11:16:50 +0200

    Implement function to get ranges

--- range_conversion.py
+++ range_conversion.py
@@ -1,3 +1,18 @@
+def get_ranges(start, stop, n):
+	start_num = str_to_num(start)
+	stop_num = str_to_num(stop)
+
+	division = (stop_num - start_num) / n
+	ranges = []
+
+	for i in range(n):
+		start_result = num_to_str(round(start_num + division * i), len(start))
+		stop_result = num_to_str(round(start_num + division * (i + 1)), len(start))
+		
+		ranges.append((start_result, stop_result))
+
+	return ranges
+
 def str_to_num(string):
 	number = 0
 	numbers = []
